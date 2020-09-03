1a2c1e6774bd8f9b647f9cd22a228f5aad0da897
Jonas Ohlsson <jonas@vansinne.se>
2020-09-03 15:52:00 +0200

    avoid hardcoding colormaps

--- REDigestGUI.py
+++ REDigestGUI.py
@@ -19,6 +19,7 @@ import platform
 import tkinter as tk
 import pandas as pd
 import matplotlib as mpl
+from matplotlib import pyplot
 if os.environ.get('DISPLAY','') == '':
     os.environ.__setitem__('DISPLAY', ':0.0')
     mpl.use('Agg')
@@ -641,36 +642,7 @@ label7T2F2.grid(column=0, row=0, sticky=tk.W, padx = 0, pady=2)
 # entry7T2F2 box = input file name
 entry7T2F2 = tk.StringVar()
 entry7T2F2_input = ttk.Combobox(colr_frameT2, width=27, textvariable=entry7T2F2)
-entry7T2F2_input['values'] = ('Accent', 'Accent_r', 'Blues', 'Blues_r', 'BrBG', 'BrBG_r', \
-'BuGn', 'BuGn_r', 'BuPu', 'BuPu_r', 'CMRmap', 'CMRmap_r', \
-'Dark2', 'Dark2_r', 'GnBu', 'GnBu_r', 'Greens', 'Greens_r', \
-'Greys', 'Greys_r', 'OrRd', 'OrRd_r', 'Oranges', 'Oranges_r', \
-'PRGn', 'PRGn_r', 'Paired', 'Paired_r', 'Pastel1', 'Pastel1_r', \
-'Pastel2', 'Pastel2_r', 'PiYG', 'PiYG_r', 'PuBu', 'PuBuGn', \
-'PuBuGn_r', 'PuBu_r', 'PuOr', 'PuOr_r', 'PuRd', 'PuRd_r', \
-'Purples', 'Purples_r', 'RdBu', 'RdBu_r', 'RdGy', 'RdGy_r', \
-'RdPu', 'RdPu_r', 'RdYlBu', 'RdYlBu_r', 'RdYlGn', 'RdYlGn_r', \
-'Reds', 'Reds_r', 'Set1', 'Set1_r', 'Set2', 'Set2_r', 'Set3', \
-'Set3_r', 'Spectral', 'Spectral_r', 'Wistia', 'Wistia_r', 'YlGn', \
-'YlGnBu', 'YlGnBu_r', 'YlGn_r', 'YlOrBr', 'YlOrBr_r', 'YlOrRd', \
-'YlOrRd_r', 'afmhot', 'afmhot_r', 'autumn', 'autumn_r', \
-'binary', 'binary_r', 'bone', 'bone_r', 'brg', 'brg_r', 'bwr', \
-'bwr_r', 'cividis', 'cividis_r', 'cool', 'cool_r', 'coolwarm',\
-'coolwarm_r', 'copper', 'copper_r', 'cubehelix', 'cubehelix_r', \
-'flag', 'flag_r', 'gist_earth', 'gist_earth_r', 'gist_gray', \
-'gist_gray_r', 'gist_heat', 'gist_heat_r', 'gist_ncar', \
-'gist_ncar_r', 'gist_rainbow', 'gist_rainbow_r', 'gist_stern', \
-'gist_stern_r', 'gist_yarg', 'gist_yarg_r', 'gnuplot', \
-'gnuplot2', 'gnuplot2_r', 'gnuplot_r', 'gray', 'gray_r', 'hot', \
-'hot_r', 'hsv', 'hsv_r', 'inferno', 'inferno_r', 'jet', 'jet_r', \
-'magma', 'magma_r', 'nipy_spectral', 'nipy_spectral_r', \
-'ocean', 'ocean_r', 'pink', 'pink_r', 'plasma', 'plasma_r', \
-'prism', 'prism_r', 'rainbow', 'rainbow_r', 'seismic', \
-'seismic_r', 'spring', 'spring_r', 'summer', 'summer_r', 'tab10', \
-'tab10_r', 'tab20', 'tab20_r', 'tab20b', 'tab20b_r', 'tab20c', \
-'tab20c_r', 'terrain', 'terrain_r', 'twilight', 'twilight_r', \
-'twilight_shifted', 'twilight_shifted_r', 'viridis', 'viridis_r', \
-'winter', 'winter_r')
+entry7T2F2_input['values'] = pyplot.colormaps()
 entry7T2F2_input.grid(column=1, row=0, sticky=tk.W, padx = 0, pady=2)
 entry7T2F2_input.current(57)
 entry7T2F2_input.focus()
